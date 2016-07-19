3774feff94a594eb139da658bfeb9fd10b29a81c
Pascelle Saint-Laurent <pascelle.saintlaurent@gmail.com>
2016-07-19 15:55:53 -0400

    now

--- js/Application.js
+++ js/Application.js
@@ -1,81 +1,45 @@
 
 $(document).ready(function(){
 
-	var randomNumber;
-	var clicks=0; 
+	$("#intro").fadeIn(2000);
+	$("#play_area").hide();
+	$("#done").hide();
 
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
-
-  	});
-
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-
-	$(".new").click(function(){
-		location.reload();
+	$("#introgspic").click(function(e){
+		e.preventDefault(); // prevent the click from happening. 
+		$("#play_area").fadeIn(); // show play area
+		$("#intro").fadeOut(); // hide the intro
 	});
 
-  	$("#guessButton").click(function(e) {
 
-  		e.preventDefault();
- 
-  		if (randomNumber == null) {
-  			randomNumber = generateNumber() 
-  		};
+(questionList).forEach(function(currentQues) { // each question in questionList Array
+	var possibleAnswers = currentQues.choices; // gabe: Array of choices for current question. Alternative syntax is: currentQues['choices']
+	$("#quesForm").append('<li class="ask">' + currentQues.ask + '</li>'); // the question. gabe: added quotes around html string
+	$("#quesForm").append('<input id="choices" type="radio">' + possibleAnswers + '<br>');
 
-  		clickCounter();
-  		compareNumbers();
+	// gabe: iterate over each possibleAnswer
+	possibleAnswers.forEach(function(possibleAnswer) {
+		var value = correntans == possibleAnswer ? 'right' : 'wrong';
+		 // gabe: added quotes around html string. listed each possibleAns
+	})
 
-  		function clickCounter() {							
-  			$("#count").html(clicks += 1);
-  		};
-  	
-  	 	function generateNumber() {	
-  			var minNumber = 1;
-  			var maxNumber = 100;
-  			var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-  				
-  			return randomNumber;
-  	 	};
-
-	  	function compareNumbers () {
-	  		var userGuess = $("#userGuess").val();   
-  	 		var difference = Math.abs(parseInt(userGuess - randomNumber));
-
-  	 		$("#guessList").append("<li>" + userGuess + "</li>");
-
-	  		function wrongNumber(message) {   
-	  			confirm(message);
-	  		};
+})
+	
 
 
-			if (difference == 0) {
-				alert ("You Guessed Correctly! Hooray!");
+	/* ----- when shepherd clicked, play_area id shown, question_area and header classes shown, others hidden ------  */
+				/* ----- question counter needs to be coded ------  */
+				/* ----- first question displayed w choices ------  */
 
-			} else if (difference >= 50 && difference <= 100) {
-				wrongNumber("You are ice cold! Try again!");
+	/* ----- when submit button pushed, answer_area class comes in ------  */
+			/*need to code evaluation of answers against one another*/
+			/* ----- Next Question button hit moves on to new question removes old ques------  */
 
-			} else if (difference >= 30 && difference <= 49) {
-				wrongNumber("You are cold! Try again!");
+	/* ----- when submit pushed on last question, done class displayed------  */
 
-			} else if (difference >= 20 && difference <= 29)  {
-				wrongNumber("You are warm! Try again!");
+			/* ----- play again button refreshes game ------  */
 
-			} else if (difference >= 10 && difference <= 19)  {
-				wrongNumber("You are hot! Try again!");
 
-			} else if (difference >= 1 && difference <= 9)  {
-				wrongNumber("You are very hot! Try again!");
-
-			} else {
-				wrongNumber("You did not enter a number between 1 and 100.  Please try again!");
-			} 
-		};
-	});
 });
 
 
