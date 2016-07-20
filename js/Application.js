aab43cd0d11cab13f13274966da723ddada66afb
Pascelle Saint-Laurent <pascelle.saintlaurent@gmail.com>
2016-07-20 17:59:43 -0400

    yes

--- js/Application.js
+++ js/Application.js
@@ -1,45 +1,69 @@
 
-$(document).ready(function(){
+$(document).ready(function loaded (){
+
+	// Intro class shown; img with instructions to click. Other classes hidden
 
 	$("#intro").fadeIn(2000);
 	$("#play_area").hide();
 	$("#done").hide();
 
-	$("#introgspic").click(function(e){
+	// User clicks on img, fades out intro class, fades in first ques via play area class
+
+	$("#introgspic").click(function introduction (e){
 		e.preventDefault(); // prevent the click from happening. 
-		$("#play_area").fadeIn(); // show play area
 		$("#intro").fadeOut(); // hide the intro
+		$("#play_area").fadeIn(); // show play area
+		$("#answer_area").hide();
 	});
 
+	//Play area
+	//Ask user a question
+	
+	var totalQuestions = questionList.length;
+	var currentQues = questionList[0];
+	var displayTheQues = $("#questions").append('<li class="ask">' + currentQues.ask + '</li>');
+	var displayAnsChoices = currentQues.choices.forEach(function getEachAnsChoice (ansChoice) {
+			$("#questions").append('<input id="choices" type="radio">' + ansChoice + '<br>')
+			});
 
-(questionList).forEach(function(currentQues) { // each question in questionList Array
-	var possibleAnswers = currentQues.choices; // gabe: Array of choices for current question. Alternative syntax is: currentQues['choices']
-	$("#quesForm").append('<li class="ask">' + currentQues.ask + '</li>'); // the question. gabe: added quotes around html string
-	$("#quesForm").append('<input id="choices" type="radio">' + possibleAnswers + '<br>');
 
-	// gabe: iterate over each possibleAnswer
-	possibleAnswers.forEach(function(possibleAnswer) {
-		var value = correntans == possibleAnswer ? 'right' : 'wrong';
-		 // gabe: added quotes around html string. listed each possibleAns
-	})
+	// displayTheQues;
+	// displayAnsChoices;
+
+	//User Submits Answer
+
+	var uponSubmit = $("#subbtn").click(function checkAns (){
+			$("#question_area").hide(); 
+			$("#answer_area").show();
+			
+			var chosenAns = $("input[id='choices']:checked").val();
+			var correctAnswer = currentQues.correctAns[0];
+
+				if (chosenAns == correctAnswer) {
+					$("#answer").text("You are correct!");
+					$("#facts").append(currentQues.info);
+				}
+
+				else {
+					$("#answer").append("Sorry, you are incorrect.  The correct answer is " + correctAnswer);
+					$("#facts").append(currentQues.info);
+				}
+		})
+
+	// User clicks for next question
+	var uponNext = $("#nextbtn").click(function nextQues (){
+			$()	
+		})
+
+
+	var quesLoop = 
+	displayTheQues.fadeIn();
+	displayAnsChoices.fadeIn();
+	uponSubmit;
+	uponNext;
+
+	
+		 
 
 })
 	
-
-
-	/* ----- when shepherd clicked, play_area id shown, question_area and header classes shown, others hidden ------  */
-				/* ----- question counter needs to be coded ------  */
-				/* ----- first question displayed w choices ------  */
-
-	/* ----- when submit button pushed, answer_area class comes in ------  */
-			/*need to code evaluation of answers against one another*/
-			/* ----- Next Question button hit moves on to new question removes old ques------  */
-
-	/* ----- when submit pushed on last question, done class displayed------  */
-
-			/* ----- play again button refreshes game ------  */
-
-
-});
-
-
