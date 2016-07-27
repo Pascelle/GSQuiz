963a37b5dbf5c8b7dbd03ac058646d071c891489
Pascelle Saint-Laurent <pascelle.saintlaurent@gmail.com>
2016-07-27 16:36:35 -0400

    latest

--- js/Application.js
+++ js/Application.js
@@ -1,46 +1,44 @@
 
-$(document).ready(function loaded (){
-
-	// Intro class shown; img with instructions to click. Other classes hidden
-
-	$("#intro").fadeIn(2000);
-	$("#play_area").hide();
-	$("#done").hide();
+$(document).ready(function docIsLoaded (){
 
 
-	$("#introgspic").click(function introduction (e){
-		e.preventDefault(); // prevent the click from happening. 
-		$("#intro").fadeOut(); // hide the intro
-		$("#play_area").fadeIn(); // show play area
-		$("#answer_area").hide();
-	});
+		$("#intro").fadeIn(2000);
+		$("#play_area").hide();
+		$("#done").hide();
 
-	var lastQuestion = questionList[4];
-	var currentQuesIndex = 0;
-	var currentQues = questionList[currentQuesIndex];
-	var displayTheQues = function () {$("#questions").append('<li class="ask">' + currentQues.ask + '</li>')};
-	var displayAnsChoices = function () {
-			currentQues.choices.forEach(function getEachAnsChoice (ansChoice) {
-			$("#questions").append('<input name="choices" type="radio" value="'+ansChoice+'">' + ansChoice + '<br>')})
-		};	
-	var removeTheQues = function() { $("#questions").empty();
-		};
-	var removeTheAns = function() { $("#answer").empty();
-		$("#facts").empty();
-		};
-	var numCorrect = 0;
-	var displayQuesCounterIndex = 1;
-	var displayQuesCounter = function () {$("#cur_quest_num").append(displayQuesCounterIndex)};
+		$("#introgspic").click(function whenPicIsClicked (e){
+			e.preventDefault(); // prevent the click from happening. 
+			$("#intro").fadeOut(); // hide the intro
+			$("#play_area").fadeIn(); // show play area
+			$("#answer_area").hide();
+		});
 
-		displayTheQues();
-		displayAnsChoices();
-		displayQuesCounter();
+		var lastQuestion = questionList[4];
+		var currentQuesIndex = 0;
+		var currentQues = questionList[currentQuesIndex];
+		var displayTheQues = function () {$("#questions").append('<li class="ask">' + currentQues.ask + '</li>')};
+		var displayAnsChoices = function () {
+				currentQues.choices.forEach(function getEachAnsChoice (ansChoice) {
+				$("#questions").append('<input name="choices" type="radio" value="'+ansChoice+'">' + ansChoice + '<br>')})
+			};	
+		var removeTheQues = function() { $("#questions").empty();
+			};
+		var removeTheAns = function() { $("#answer").empty();
+			$("#facts").empty();
+			};
+		var numCorrect = 0;
+		var displayQuesCounterIndex = 1;
+		var displayQuesCounter = function () {$("#cur_quest_num").append(displayQuesCounterIndex)};
 
-
-	var uponSubmit = $("#subbtn").click(function checkAns (){
+			displayTheQues();
+			displayAnsChoices();
+			displayQuesCounter();
+	
+	
+		$("#subbtn").click(function checkAns (){
 			$("#question_area").hide(); 
 			$("#answer_area").show();
-			
+				
 			var chosenAns = $("input[name='choices']:checked").val();
 			var correctAnswer = currentQues.correctAns[0];
 
@@ -57,45 +55,37 @@ $(document).ready(function loaded (){
 				}
 		});
 
-	// User clicks for next question
-	var uponNext = $("#nextbtn").click(function nextQues (){
-		$("#answer_area").fadeOut();
 
-		if (lastQuestion == currentQues) {
-				$("#done").fadeIn();
-			$("#rsbtn").click(function playAgain (){
-				location.reload();
-			})
-		}
-		
-		else {
-			removeTheQues();
-			removeTheAns();
-
-			currentQuesIndex++; //increment	
-			currentQues = questionList[currentQuesIndex];//re-reference to change the current question
-
-			displayQuesCounterIndex += 1;
-			$("#cur_quest_num").empty();
-			displayQuesCounter();
-
-			$("#question_area").show();
-			displayTheQues();
-			displayAnsChoices();
-			}		
-	});
-
-
-
-/*
-	var quesLoop = 
-	displayTheQues.fadeIn();
-	displayAnsChoices.fadeIn();
-	uponSubmit;
-	uponNext;
-*/
 	
-		 
+		var uponNext = $("#nextbtn").click(function nextQues (){
+			$("#answer_area").hide();
+
+			if (lastQuestion == currentQues) {
+					$("#done").fadeIn();
+				$("#rsbtn").click(function playAgain (){
+					location.reload();
+				})
+			}
+			
+			else {
+				removeTheQues();
+				removeTheAns();
+
+				currentQuesIndex++; //increment	
+				currentQues = questionList[currentQuesIndex];//re-reference to change the current question
+
+				displayQuesCounterIndex += 1;
+				$("#cur_quest_num").empty();
+				displayQuesCounter();
+
+				$("#question_area").show();
+				displayTheQues();
+				displayAnsChoices();
+			}		
+		});
+
+
+	
 
 })
 	
